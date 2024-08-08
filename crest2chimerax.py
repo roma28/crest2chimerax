f941bab9ec4d3a1b09f3e0a591a21a055d39b369
RI <ichenkoroma@gmail.com>
2024-08-08 10:52:24 +0200

    Add energy cutoff

--- crest2chimerax.py
+++ crest2chimerax.py
@@ -4,8 +4,9 @@ import logging
 import math
 import os
 
-R = 1.9872036e-3
-T = 298.15
+R = 1.9872036e-3  # kcal/mol
+T = 298.15  # K
+Eh_to_kcalpermol = 625.5
 
 
 @dataclasses.dataclass
@@ -19,11 +20,14 @@ class Conformer:
 logging.basicConfig(level=logging.DEBUG)
 
 argparser = argparse.ArgumentParser(prog='crest2chimerax',
-                                    description='A script that takes CREST conformer ensemble and produces ChimeraX '
-                                                'script to visualize', )
+                                    description='A script that takes CREST .xyz conformer ensemble and produces '
+                                                'ChimeraX script to visualize', )
 argparser.add_argument('infile', help='CREST file')
-argparser.add_argument('outfile')
-argparser.add_argument('-r', '--refatoms', default='*', help='refatoms string as used in ChimeraX align command')
+argparser.add_argument('outfile', help='ChimeraX .cxc file')
+argparser.add_argument('-r', '--refatoms', default='*',
+                       help='Refatoms string as used in ChimeraX align command (* by default)')
+argparser.add_argument('-e', '--energy', type=float, default=6.0,
+                       help='Cutoff energy in kcal/mol (6 kcla/mol by default)')
 args = argparser.parse_args()
 
 infile = os.path.abspath(args.infile)
@@ -37,6 +41,9 @@ try:
     os.mkdir('./conformers')
 except FileExistsError:
     logging.error('Directory "conformers" already exists and will be overwritten!')
+    answer = input("Continue? ")
+    if not answer.upper() in ["Y", "YES"]:
+        exit(-1)
 
 conformers = []
 
@@ -54,17 +61,20 @@ with open(infile, 'r') as infile:
             logging.debug(f'Writing conformer {i} in file {os.path.abspath(f'./conformers/c{i}.xyz')}')
             c.writelines(lines[i * (n_atoms + 2):(i + 1) * (n_atoms + 2)])
 
-e_min = min([c.energy_Eh for c in conformers])
+del lines
+
+e_min = min(conformers, key=lambda x: x.energy_Eh).energy_Eh
 for c in conformers:
-    c.energy_relative_kcalpermol = (c.energy_Eh - e_min) * 625.5
+    c.energy_relative_kcalpermol = (c.energy_Eh - e_min) * Eh_to_kcalpermol
     c.population_relative = math.exp(-c.energy_relative_kcalpermol / R / T)
+
+conformers = filter(lambda x: x.energy_Eh < e_min, conformers)
 
 with open(outfile, 'w') as outfile:
     outfile.writelines(f'cd {workdir}\n')
     for i, c in enumerate(conformers):
         outfile.writelines(f'open {c.fname}\n')
         outfile.writelines(f'transparency #{i + 1} {100 - c.population_relative * 100} target ab\n')
-
         if i == 0:
             continue
-        outfile.writelines(f'align #{i+1}@{args.refatoms} toAtoms #1@{args.refatoms}\n')
+        outfile.writelines(f'align #{i + 1}@{args.refatoms} toAtoms #1@{args.refatoms}\n')
